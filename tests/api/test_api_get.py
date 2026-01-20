6d27e4da2d8758ae5ed1a53150ee105d0e6625c2
nicetnp <nicetnp1998@gmail.com>
2026-01-20 19:35:26 +0700

    test api get

---
+++ tests/api/test_api_get.py
@@ -0,0 +1,18 @@
+def test_api_get(playwright):
+    request = playwright.request.new_context(
+        extra_http_headers={
+            "Accept":"application/json",
+            "Authorization": "Bearer YOUR_ACCESS_TOKEN",
+            "x-api-key": "reqres-free-v1"
+        }
+    )
+    response = request.get("https://reqres.in/api/users?page=2")
+
+    assert response.status == 200
+    json_data = response.json()
+    print(json_data)
+
+    assert json_data["page"] == 2
+    request.dispose()
+
+    print("Test completed successfully")
